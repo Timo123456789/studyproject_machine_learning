a9b491f45835cdf23dfc6a3ca5730277b30cbc33
MaxiElfers <maximilian-elfers@gmx.de>
2024-01-18 00:08:06 +0100

    some more preprocessing functions

--- scripts/preprocessing/rgbPreprocessing.py
+++ scripts/preprocessing/rgbPreprocessing.py
@@ -28,23 +28,29 @@ def background_Sub(vid, name, output_path):
         print(str(frame_count) + " / " + str(total_frames))
     cap.release()
 
-    # if typ == "colorManipu":
-    #     img = Image.open('../insects/train/images_rgb/img_001_002910.png')
-    #     M = np.asarray(img)
-    #     blue = M[:, :, 0]
-    #     green = M[:, :, 1]
-    #     red = M[:, :, 2]
-    #
-    #     cv2.imwrite("red.jpg", red)
-    #     cv2.imwrite("green.jpg", green)
-    #     cv2.imwrite("blue.jpg", blue)
-    #
-    #     img2 = Image.open("../resultsPreprocessing/subtraction.jpg")
-    #     M2 = np.asarray(img2)
-    #     blue2 = M2[:, :, 0]
-    #     enhanced_image = np.stack((red, green, blue2), axis=-1).astype(np.uint8)
-    #     enhanced_image = Image.fromarray(enhanced_image)
-    #     enhanced_image.save("combi channels.jpg")
+def rgbToHsv(vid, name, output_path):
+    output = os.path.join(output_path, name)
+    cap = cv2.VideoCapture(vid)
+    size = (int(cap.get(cv2.CAP_PROP_FRAME_WIDTH)),
+            int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT)))
+    fps = cap.get(cv2.CAP_PROP_FPS)  # Get the frame rate of the input video
+    fourcc = cv2.VideoWriter_fourcc(*'MP4V')
+    video = cv2.VideoWriter(output, fourcc, fps, size)
+    total_frames = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+    frame_count = 0
+
+    while True:
+        ret, frame = cap.read()
+        if not ret or frame_count >= total_frames:
+            break
+
+        hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+        hsv[:, :, 0] = 179
+        video.write(hsv)
+
+        frame_count += 1
+        print(str(frame_count) + " / " + str(total_frames))
+    cap.release()
 
 
 def substract(vid, name, output_path):
@@ -107,11 +113,47 @@ def time_offset_by1(vid, name, output_path):
     
     cap.release()
 
+def time_offset_by10(vid, name, output_path):
+    output = os.path.join(output_path, name)
+    cap = cv2.VideoCapture(vid)
+    size = (int(cap.get(cv2.CAP_PROP_FRAME_WIDTH)),
+            int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT)))
+    fps = cap.get(cv2.CAP_PROP_FPS)  # Get the frame rate of the input video
+    fourcc = cv2.VideoWriter_fourcc(*'MP4V')
+    video = cv2.VideoWriter(output, fourcc, fps, size)
+    total_frames = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+    frame_count = 0
+
+    frames = [None] * 20  # Initialize a list with 20 None elements
+
+    while True:
+        ret, frame = cap.read()
+        if not ret or frame_count >= total_frames:
+            break
+
+        frames[frame_count % 20] = frame  # Store the current frame in the list
+
+        if frame_count >= 19:  # Start writing to the video after 20 frames have been read
+            frame_minus10 = frames[(frame_count - 10) % 20]  # Get the frame from 10 frames ago
+            frame_minus20 = frames[(frame_count - 20) % 20]  # Get the frame from 20 frames ago
+
+            # Combine the frames into a single image where each frame is a different color channel
+            frame[:, :, 2] = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)  # red
+            frame[:, :, 1] = cv2.cvtColor(frame_minus10, cv2.COLOR_BGR2GRAY)  # green
+            frame[:, :, 0] = cv2.cvtColor(frame_minus20, cv2.COLOR_BGR2GRAY)  # blue
+            video.write(frame)
+
+        frame_count += 1
+        print(str(frame_count) + " / " + str(total_frames))
+
+    cap.release()
+
+
 
 if __name__ == "__main__":
     # Setup
     video_dir = "videos_new_cutted/"
-    output_dir = '../../../../scratch/tmp/melfers/videos_new_offsetBy1/'
+    output_dir = '../../../../scratch/tmp/melfers/videos_new_hsvHUE/'
     # video_dir = "../../videos_new_cutted/"
     # output_dir = '../../videos_new_test/'
 
@@ -133,7 +175,7 @@ if __name__ == "__main__":
         # Für jede Datei im Ordner
         for video_file in os.listdir(video_dir_deep):
             vid_file = os.path.join(video_dir_deep, video_file)
-            time_offsetby1(vid_file, video_file, output_dir_deep)
+            rgbToHsv(vid_file, video_file, output_dir_deep)
 
 
 
